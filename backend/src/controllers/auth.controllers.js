177c14991c0499f4ca9ecb5dce1f9c5c9fa05842
mdsalmanfarsee <far90986@gmail.com>
2025-03-23 11:35:59 +0530

    add authentication route

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,3 +1,110 @@
-import User from "../models/user.model";
-import bcript from "bcryptjs";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../lib/utils.js";
+import cloudinary from "../lib/cloudinary.js";
+
+
+
+
+const signup = async (req, res) => {
+    const { fullName, email, password } = req.body;
+    try {
+        if (!fullName || !email || !password) {
+            return res.status(400).json({ message: "Please fill all the fields" });
+        }
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be atleast 6 characters long" });
+        }
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const salt = await bcrypt.genSalt(12);
+        const hassedPassword = await bcrypt.hash(password, salt);
+        const newUser = new User({
+            fullName: fullName,
+            email: email,
+            password: hassedPassword
+        });
+
+        if (newUser) {
+            //generate jwt token
+            generateToken(newUser._id, res);
+            await newUser.save();
+            return res.status(201).json({
+                message: "User created successfully",
+                _id: newUser._id,
+                profilepic: newUser.profilepic,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                createdAt: newUser.createdAt,
+            });
+
+        }
+        else {
+            return res.status(400).json({ message: "Something went wrong" });
+        }
+
+    } catch (error) {
+        return res.status(400).json({ message: "Something went wrong in signup" });
+    }
+}
+
+
+
+
+
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "User does not exist" });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        generateToken(user._id, res);
+        return res.status(200).json({
+            message: "User logged in successfully",
+            _id: user._id,
+            profilepic: user.profilepic,
+            fullName: user.fullName,
+            email: user.email,
+            createdAt: user.createdAt,
+        });
+
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error in login" });
+    }
+}
+
+
+
+
+
+
+
+const logout = async (req, res) => {
+    const val1 = process.env.OPERATION === "production";
+    const val2 = process.env.OPERATION === "production" ? "None" : "Lax";
+
+    try {
+        res.clearCookie("jwt", {
+            httpOnly: true,
+            secure: process.env.OPERATION === "production",
+            sameSite: process.env.OPERATION === "production" ? "None" : "Lax",
+        });
+        return res.status(200).json({ message: "User logged out successfully" });
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+
+export { signup, login, logout };
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,51 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import { connectDB } from './lib/dbConnection.js';
+import authRouter from './routes/auth.route.js';
 
 
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+
+//connect to database
 connectDB();
+
+
+//set up middlewares
+const PORT = process.env.PORT || 5000;
+const mode = process.env.OPERATION;
+const FRONTEND_URL = mode === "production" ? process.env.FRONTEND_URL : 'http://localhost:5173';
+
+
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
+app.use(cookieParser());
+app.use(cors({
+    origin: [FRONTEND_URL],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}))
+
+
+
+
+
+//routes
+app.use('/api/auth', authRouter);
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running at port ${PORT}`);
+    console.log(`Server is running at  http://localhost:${PORT}`);
 })
