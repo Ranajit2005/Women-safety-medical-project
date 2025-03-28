14cd5702c5b80d06afe2aa79c762a74198ad843a
Ranajit2005 <18ranajit2005@gmail.com>
2025-03-28 20:01:54 +0530

    article schema

---
+++ backend/src/models/article.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+
+const articleSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    content:{
+        type: String,
+        required: true
+    },
+    date:{
+        type: Date,
+        default: Date.now()
+    },
+    doctorName:{
+        type:String,
+        required: true
+    }
+})
+
+const Article = mongoose.model('Article', articleSchema);
+export default Article;
