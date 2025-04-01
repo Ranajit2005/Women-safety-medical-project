cb392b9cafe8096c5d731f15ff989996115e9521
FriaElegante <mail2sanjanaban@gmail.com>
2025-04-01 22:36:47 +0530

    icon images updated

--- frontend/src/components/Circle.jsx
+++ frontend/src/components/Circle.jsx
@@ -4,25 +4,25 @@ function Circle() {
   return (
     <div className="flex flex-wrap justify-around px-5 sm:px-7 gap-3 sm:gap-5  ">
       <CircleCard
-        imageSrc="backgroundSignUp.jpg"
+        imageSrc="/ICONS/ICON1.png"
         title="Women health"
         hoverText="Learn about essential health tips and well-being."
         link="#"
       />
       <CircleCard
-        imageSrc="backgroundSignUp.jpg"
+        imageSrc="/ICONS/ICON2.jpg"
         title="Substances and women's body"
         hoverText="Understand how substances affect women's health."
         link="#"
       />
       <CircleCard
-        imageSrc="backgroundSignUp.jpg"
+        imageSrc="/ICONS/ICON3.jpg"
         title="Community and blogs"
         hoverText="Join the conversation and share your experiences."
         link="#"
       />
       <CircleCard
-        imageSrc="backgroundSignUp.jpg"
+        imageSrc="/ICONS/ICON4.jpg"
         title="Women products"
         hoverText="Explore curated products designed for women."
         link="#"
