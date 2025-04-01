90e3f2e6606b9c31b392f6958ede75d45df2191e
FriaElegante <mail2sanjanaban@gmail.com>
2025-04-01 22:51:55 +0530

    icons updated

--- frontend/src/components/Circle.jsx
+++ frontend/src/components/Circle.jsx
@@ -4,41 +4,25 @@ function Circle() {
   return (
     <div className="flex flex-wrap justify-around px-5 sm:px-7 gap-3 sm:gap-5  ">
       <CircleCard
-<<<<<<< HEAD
         imageSrc="/ICONS/ICON1.png"
-=======
-        imageSrc="/bg/bg1.jpg"
->>>>>>> 8bfa023a95c9f5b0d147411cc18943adeb70279c
         title="Women health"
         hoverText="Know about different diseases affecting the female body."
         link="/diseases"
       />
       <CircleCard
-<<<<<<< HEAD
         imageSrc="/ICONS/ICON2.jpg"
-=======
-        imageSrc="bg/bg1.jpg"
->>>>>>> 8bfa023a95c9f5b0d147411cc18943adeb70279c
         title="Substances and women's body"
         hoverText="Understand how substances affect women's health."
         link="/subAndBody"
       />
       <CircleCard
-<<<<<<< HEAD
         imageSrc="/ICONS/ICON3.jpg"
-=======
-        imageSrc="/bg/bg1.jpg"
->>>>>>> 8bfa023a95c9f5b0d147411cc18943adeb70279c
         title="Community and blogs"
         hoverText="Join the conversation and share your experiences."
         link="#"
       />
       <CircleCard
-<<<<<<< HEAD
         imageSrc="/ICONS/ICON4.jpg"
-=======
-        imageSrc="/bg/bg1.jpg"
->>>>>>> 8bfa023a95c9f5b0d147411cc18943adeb70279c
         title="Women products"
         hoverText="Explore curated products designed for women."
         link="/product"
